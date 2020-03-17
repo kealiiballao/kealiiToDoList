c28e6a6caa2094f087191745abcf1a3e07894077
Kealii Ballao <ballao@hawaii.edu>
2020-03-17 13:49:10 -1000

    Fixed to default to weekday page and create weekend page

--- index.js
+++ index.js
@@ -1,39 +1,31 @@
-//base express code.
-
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js"); //brings in date.js module
 const app = express();
-let items = [];
-let workItems = [];
-let funItems = [];
+let weekdayItems = [];
+let weekendItems = [];
 app.set("view engine", "ejs");
 //must be below the app constant.
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.get("/", function(req, res) {
-  let day = date.getDay(); //use date() module
-  res.render("list", { listTitle: day, newListItems: items });
+app.get("/weekday", function (req, res) {
+  res.render("list", {
+    listTitle: "Work Day List",
+    newListItems: weekdayItems
+  });
 });
-app.post("/", function(req, res) {
+app.post("/", function (req, res) {
   let item = req.body.newTask;
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else if (req.body.list ==="Fun") {
-    funItems.push(item);
-    res.redirect("/fun");
+  if (req.body.list === "Weekday") {
+    weekdayList.push(item);
+    res.redirect("/workday");
   } else {
-    items.push(item);
-    res.redirect("/");
+    weekendItems.push(item);
+    res.redirect("/weekend");
   }
 });
-app.get("/work", function(req, res) {
-  res.render("list", { listTitle: "Work", newListItems: workItems });
+app.get("/weekend", function (req, res) {
+  res.render("list", { listTitle: "Weekend", newListItems: weekendItems });
 });
-app.get("/fun", function(req, res) {
-  res.render("list", { listTitle: "Fun", newListItems: funItems });
-});
-app.listen(3003, function() {
+app.listen(3003, function () {
   console.log("Server started on port 3003.");
 });
